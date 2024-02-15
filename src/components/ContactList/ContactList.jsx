454fea8fc89f619c6daf209910f89988f86407a7
MarznaHuzei <marinka777xx@gmail.com>
2024-02-15 14:20:06 +0100

    fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,31 +1,3 @@
-// import React from 'react';
-
-// // Компонент списку контактів
-// const ContactList = ({ contacts, onRemoveContact }) => (
-//   <div>
-//     <ul>
-//       {contacts.map(contact => (
-//         <li key={contact.id}>
-//           {contact.name + ' : ' + contact.number}
-//           {
-//             // Кнопка видалення контакту
-//             <button
-//               type="button"
-//               name="delete"
-//               onClick={() => onRemoveContact(contact.id)}
-//             >
-//               delete
-//             </button>
-//           }
-//         </li>
-//       ))}
-//     </ul>
-//   </div>
-// );
-
-// export default ContactList;
-
-// ContactList.js
 import React from 'react';
 import Contact from '../Contact/Contact';
 import styles from './ContactList.module.css';
